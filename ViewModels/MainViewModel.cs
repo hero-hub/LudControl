6270699f5b2be0e283cc2d6a2acf3b52c3a8764b
FMVACHOO <vlad-efimov-2015@mail.ru>
2025-05-19 10:02:08 +0300

    не работает прослущивание по UDP

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -2,58 +2,41 @@
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.ComponentModel;
-using System.IO;
-using LudControl.Models;
-using OxyPlot;
-using OxyPlot.Series;
-
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace LudControl
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : INotifyPropertyChanged, IDisposable
     {
-        private readonly TcpCommandServer _tcpServer; // TCP-сокет
-        private readonly UdpDataServer _udpServer; // UDP-сокет
-        private readonly UdpDataClient _udpClient; // UDP-клиент для работы с сокетом
-
-        private PlotModel _plotModel;
-        private string _commandViewer = string.Empty;
-        private bool _isConnected = false;
+        private readonly TcpClient _tcpClient;
+        private readonly UdpClient _udpClient;
+        private readonly IPEndPoint _serverEndPoint;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private string _commandViewer;
 
         public MainViewModel()
         {
-            _tcpServer = new TcpCommandServer(62125);
-            _udpServer = new UdpDataServer(62126);
-            _udpClient = new UdpDataClient("127.0.0.1", 62126);
+            _udpClient = new UdpClient(); // Автоматический выбор порта
+            _serverEndPoint = new IPEndPoint(IPAddress.Loopback, 0);
+            _tcpClient = new TcpClient();
 
-            _tcpServer.CommandReceived += OnTcpCommandReceived;
-            _udpServer.DataReceived += OnUdpDataReceived;
+            StartCommand = new RelayCommand(_ => Start());
+            AddMeCommand = new RelayCommand(_ => Subscribe("ADD_ME"));
+            DelMeCommand = new RelayCommand(_ => Subscribe("DELL_ME"));
 
-            //SetupPlot();
-            Start = new RelayCommand(async _ => await StartServerAsync(), _ => !_isConnected);
-            Stop = new RelayCommand(_ => StopServer(), _ => _isConnected);
-            Exit = new RelayCommand(_ => ExitApplication(), _ => true);
-
-            AddMeCommand = new RelayCommand(_ => _udpClient.SubscribeAsync());
-            DelMeCommand = new RelayCommand(_ => _udpClient.UnsubscribeAsync());
+            // Запускаем асинхронный приёмник в фоновом потоке
+            Task.Run(() => UdpReceiverAsync(_cts.Token));
         }
 
-        public ICommand Start { get; }
-        public ICommand Stop { get; }
-        public ICommand Exit { get; }
+        public ICommand StartCommand { get; }
+        public ICommand StopCommand { get; }
+        public ICommand ExitCommand { get; }
         public ICommand AddMeCommand { get; }
         public ICommand DelMeCommand { get; }
 
-        public PlotModel PlotModel
-        {
-            get => _plotModel;
-            set
-            {
-                _plotModel = value;
-                OnPropertyChanged(nameof(PlotModel));
-            }
-        }
-        public string CommandViewer // Поле вывода команд/ошибок
+        public string CommandViewer
         {
             get => _commandViewer;
             set
@@ -63,47 +46,57 @@ namespace LudControl
             }
         }
 
-        private async Task StartServerAsync()
+        private void Subscribe(string command)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(command);
+            _udpClient.Send(data, data.Length, _serverEndPoint);
+        }
+
+        private void Start()
+        {
+            if (!_tcpClient.Connected)
+            _tcpClient.Connect("127.0.0.1", 62125);
+
+            NetworkStream stream = _tcpClient.GetStream();
+            byte[] data = Encoding.UTF8.GetBytes("Start");
+            stream.Write(data, 0, data.Length);
+        }
+
+        private async Task UdpReceiverAsync(CancellationToken cancellationToken)
         {
             try
             {
-                await _tcpServer.StartAsync();
-                _ = _udpServer.StartAsync();
-                _isConnected = true;
-                CommandViewer += "Сервер запущен\n";
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    UdpReceiveResult result = await _udpClient.ReceiveAsync();
+                    byte[] data = result.Buffer;
+                    string message = $"Первые 10 байт: {string.Join(", ", data.Take(10))}\n";
+                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        CommandViewer += message;
+                    });
+                }
             }
             catch (Exception ex)
             {
-                CommandViewer += $"Ошибка запуска: {ex.Message}\n";
+                System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                {
+                    CommandViewer += $"Ошибка в UdpReceiver: {ex.Message}\n";
+                });
             }
-        }
-        private void StopServer()
-        {
-            _tcpServer.Stop();
-            _udpServer.Stop();
-            _isConnected = false;
-            CommandViewer += "Сервер остановлен\n";
-        }
-        private void ExitApplication()
-        {
-            StopServer();
-            System.Windows.Application.Current.Shutdown();
-        }
-
-        private void OnTcpCommandReceived(string command)
-        {
-            CommandViewer += $"[TCP] Получена команда: {command}\n";
-        }
-
-        private void OnUdpDataReceived(string data)
-        {
-            CommandViewer += $"[UDP Data] {data}\n";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Dispose()
+        {
+            _cts.Cancel();
+            _tcpClient?.Close();
+            _udpClient?.Close();
         }
     }
 }
