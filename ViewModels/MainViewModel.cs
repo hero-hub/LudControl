1bc7fe81762437c7a774d2bf847815ce5230924a
FMVACHOO <vlad-efimov-2015@mail.ru>
2025-05-26 13:09:48 +0300

    Реализовано два потока. Правильно настроено преобразование данных. команды stop и exit не принимаются сервером (или не отправляются)

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text;
 using OxyPlot;
 using OxyPlot.Series;
+using System.Collections.Concurrent;
 
 namespace LudControl
 {
@@ -18,21 +19,25 @@ namespace LudControl
         private readonly IPEndPoint _serverEndPoint;
         private readonly CancellationTokenSource _cts = new CancellationTokenSource();
         private string _commandViewer;
+        private readonly ConcurrentQueue<UInt16[]> _dataBuffer = new ConcurrentQueue<UInt16[]>();
+        private const int BufferSize = 500;
 
         public MainViewModel()
         {
-            _udpClient = new UdpClient(); // Привязка к локальному порту 62127
+            _udpClient = new UdpClient(0); // 
             _udpClient.Connect("127.0.0.1", 62126);
             _serverEndPoint = new IPEndPoint(IPAddress.Any, 62126); // Серверный порт
             _tcpClient = new TcpClient();
 
             SetupPlot();
             StartCommand = new RelayCommand(async _ => await ManagerAsync("start"));
-            StopCommand = new RelayCommand(_ => Dispose());
+            StopCommand = new RelayCommand(async _ => await ManagerAsync("stop"));
+            ExitCommand = new RelayCommand(_ => Dispose());
             AddMeCommand = new RelayCommand(async _ => await SubscribeAsync("ADD_ME"));
             DelMeCommand = new RelayCommand(async _ => await SubscribeAsync("DELL_ME"));
 
             Task.Run(() => UdpReceiverAsync(_cts.Token));
+            Task.Run(() => RenderGraphAsync(_cts.Token));
         }
 
         public ICommand StartCommand { get; }
@@ -81,11 +86,11 @@ namespace LudControl
         {
             try
             {
-                if (!_tcpClient.Connected)
+                if (!_tcpClient.Connected && command == "start")
                 {
                     await _tcpClient.ConnectAsync("127.0.0.1", 62125);
                 }
-
+                
                 NetworkStream stream = _tcpClient.GetStream();
                 byte[] data = Encoding.UTF8.GetBytes(command);
                 await stream.WriteAsync(data, 0, data.Length);
@@ -105,19 +110,25 @@ namespace LudControl
             {
                 while (!cancellationToken.IsCancellationRequested)
                 {
-                    IPEndPoint dd = new IPEndPoint(IPAddress.Any, 62126);
+                    IPEndPoint dd = new IPEndPoint(IPAddress.Any, 0);
                     byte[] buffer = _udpClient.Receive(ref dd);
-
+                    
                     UInt16[] data = new UInt16[buffer.Length / 2];
                     for (int i = 0; i < data.Length; i++)
                     {
                         data[i] = (UInt16)((buffer[i * 2 + 1] << 8) | buffer[i * 2]);
                     }
 
-                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    while (_dataBuffer.Count >= BufferSize)
                     {
-                        PlotSignal(data);
-                    });
+                        _dataBuffer.TryDequeue(out _);
+                    }
+                    _dataBuffer.Enqueue(data);
+
+                    //System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    //{
+                    //    PlotSignal(data);
+                    //});
 
                 }
             }
@@ -128,6 +139,44 @@ namespace LudControl
                     CommandViewer += $"Ошибка в UdpReceiver: {ex.Message}\n";
                 });
             }
+        }
+
+
+        private async Task RenderGraphAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                if (_dataBuffer.TryPeek(out UInt16[] data))
+                {
+                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        PlotSignal(data);
+                    });
+                }
+                await Task.Delay(1);
+            }
+        }
+
+        private void PlotSignal(UInt16[] values)
+        {
+            _plotModel.Series.Clear();
+            var series = new LineSeries();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Int16 result = (short)((values[i] > 2047) ? values[i] - 4096 : values[i]);
+                float finalValue = (float)(result * 1.75 / 4096.0);
+                series.Points.Add(new DataPoint(i, finalValue));
+            }
+
+            _plotModel.Series.Add(series);
+            _plotModel.InvalidatePlot(true);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void SetupPlot()
@@ -160,29 +209,6 @@ namespace LudControl
                 Title = "МКС"
             });
         }
-
-        private void PlotSignal(UInt16[] values)
-        {
-            PlotModel.Series.Clear();
-            var series = new LineSeries();
-
-            for (int i = 0; i < values.Length; i++)
-            {
-                int result = (values[i] > 2047) ? values[i] - 4096 : values[i];
-                float finalValue = (float)(result * 1.75 / 4096.0);
-                series.Points.Add(new DataPoint(i, finalValue));
-            }
-
-            PlotModel.Series.Add(series);
-            PlotModel.InvalidatePlot(true);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         public void Dispose()
         {
             _cts.Cancel();
